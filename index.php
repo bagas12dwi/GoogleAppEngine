c76999ba5acbcfc5e5d4a64f642b67f7bc98a2c3
bagas12dwi <bag12dwi.s@gmail.com>
2023-10-31 18:31:45 +0700

    updated index

--- index.php
+++ index.php
@@ -1,245 +1,191 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Risha Portofolio</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
 
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="./assets/css/style.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
-    />
-  </head>
+<head>
+  <title>Risha Portofolio</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-  <body>
-    <header>
-      <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand" href="#">RishaKr</a>
-          <button
-            class="navbar-toggler d-lg-none"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto ms-auto mt-2 mt-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="#intro" aria-current="page"> Home </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pendidikan" aria-current="page">
-                  Pendidikan
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#pengalaman" aria-current="page">
-                  Pengalaman
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#prestasi" aria-current="page">
-                  Prestasi
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#kemampuan" aria-current="page">
-                  kemampuan
-                </a>
-              </li>
-            </ul>
-          </div>
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
+  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">RishaKr</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto ms-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#intro" aria-current="page"> Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#pendidikan" aria-current="page">
+                Pendidikan
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#pengalaman" aria-current="page">
+                Pengalaman
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#prestasi" aria-current="page">
+                Prestasi
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#kemampuan" aria-current="page">
+                kemampuan
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </header>
-    <main>
-      <section id="intro">
-        <div class="p-5 mb-4 bg-dark text-white">
-          <div class="container py-5">
-            <div class="row g-3">
-              <div class="col-md-8 col-sm-12 info">
-                <h1 class="display-5 fw-bold">Risha Kurnia Fitri</h1>
-                <p class="fs-5">
-                  Saya seorang mahasiswa aktif yang memiliki ketertarikan pada
-                  bidang Event. Terbiasa menggunakan aplikasi administrasi
-                  seperti Microsoft Office dan Google Docs. Disiplin dan mampu
-                  bekerja dengan baik dalam tim. Pernah aktif dalam kegiatan
-                  organisasi mahasiswa dan pengalaman project seperti Program
-                  Mahasiswa Wirausaha.
-                </p>
-                <div class="d-flex">
-                  <a
-                    href="http://instagram.com/rishakr"
-                    class="nav-link fs-4"
-                    target="_blank"
-                  >
-                    <i class="bi bi-instagram"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="col-md-4 col-sm-12">
-                <div class="d-flex align-items-center justify-content-center">
-                  <img
-                    src="./assets/images/profil.png"
-                    alt=""
-                    class="rounded-circle w-75"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="pendidikan">
-        <div class="container px-4 py-3" id="featured-3">
-          <h2 class="pb-2 border-bottom">Pendidikan</h2>
-          <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-mortarboard-fill"></i>
-              </div>
-              <h3 class="fs-2 text-body-emphasis">SDN Perak Barat</h3>
-              <p>2009 - 2014</p>
-            </div>
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-mortarboard-fill"></i>
-              </div>
-              <h3 class="fs-2 text-body-emphasis">SMPN 2 Surabaya</h3>
-              <p>2005 - 2017</p>
-            </div>
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-mortarboard-fill"></i>
-              </div>
-              <h3 class="fs-2 text-body-emphasis">SMAN 7 Surabaya</h3>
-              <p>2018 - 2020</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="pengalaman">
-        <div class="container px-4 py-3" id="featured-3">
-          <h2 class="pb-2 border-bottom">Pengalaman Organisasi</h2>
-          <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-people-fill"></i>
-              </div>
-              <h3 class="fs-4 text-body-emphasis">
-                Karang Taruna Kampung Gurami, Perak Barat
-              </h3>
-              <p>2015 - 2017</p>
-            </div>
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-people-fill"></i>
-              </div>
-              <h3 class="fs-4 text-body-emphasis">
-                Paduan Suara SMAN 7 Surabaya
-              </h3>
-              <p>2018 - 2019</p>
-            </div>
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-people-fill"></i>
-              </div>
-              <h3 class="fs-4 text-body-emphasis">
-                Himpunan Mahasiswa Prodi Manajemen Informatika
-              </h3>
-              <p>2021 - 2022</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="prestasi">
-        <div class="container px-4 py-3" id="featured-3">
-          <h2 class="pb-2 border-bottom">Prestasi</h2>
-          <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div class="feature col">
-              <div
-                class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-success bg-gradient fs-2 mb-3 px-2 rounded"
-              >
-                <i class="bi bi-stars"></i>
-              </div>
-              <h3 class="fs-2 text-body-emphasis">Non-Akademik</h3>
-              <p>Peserta Paduan Suara HUT RI ke-74, Provinsi Jawa Timur</p>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id="kemampuan">
-        <div class="container px-4 py-3" id="featured-3">
-          <h2 class="pb-2 border-bottom">Kemampuan</h2>
-          <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-            <div class="feature col">
-              <div
-                class="progress"
-                role="progressbar"
-                aria-label="Example with label"
-                aria-valuenow="100"
-                aria-valuemin="0"
-                aria-valuemax="100"
-              >
-                <div class="progress-bar" style="width: 100%">100%</div>
-              </div>
-              <h3 class="fs-5 text-body-emphasis mt-2">Expert</h3>
-              <p>
-                Mengoperasikan Microsoft Office (Ms.Word, Ms. Excel, Ms. Power
-                Point)
+      </div>
+    </nav>
+  </header>
+  <main>
+    <section id="intro">
+      <div class="p-5 mb-4 bg-dark text-white">
+        <div class="container py-5">
+          <div class="row g-3">
+            <div class="col-md-8 col-sm-12 info">
+              <h1 class="display-5 fw-bold">Risha Kurnia Fitri</h1>
+              <p class="fs-5">
+                Saya seorang mahasiswa aktif yang memiliki ketertarikan pada
+                bidang Event. Terbiasa menggunakan aplikasi administrasi
+                seperti Microsoft Office dan Google Docs. Disiplin dan mampu
+                bekerja dengan baik dalam tim. Pernah aktif dalam kegiatan
+                organisasi mahasiswa dan pengalaman project seperti Program
+                Mahasiswa Wirausaha.
               </p>
+              <div class="d-flex">
+                <a href="http://instagram.com/rishakr" class="nav-link fs-4" target="_blank">
+                  <i class="bi bi-instagram"></i>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-4 col-sm-12">
+              <div class="d-flex align-items-center justify-content-center">
+                <img src="assets/images/profil.png" alt="" class="rounded-circle w-75" />
+              </div>
             </div>
           </div>
         </div>
-      </section>
-    </main>
-    <footer>
-      <!-- place footer here -->
-    </footer>
-    <!-- Bootstrap JavaScript Libraries -->
-    <script
-      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-      integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
-      crossorigin="anonymous"
-    ></script>
+      </div>
+    </section>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
-      integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+    <section id="pendidikan">
+      <div class="container px-4 py-3" id="featured-3">
+        <h2 class="pb-2 border-bottom">Pendidikan</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-mortarboard-fill"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">SDN Perak Barat</h3>
+            <p>2009 - 2014</p>
+          </div>
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-mortarboard-fill"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">SMPN 2 Surabaya</h3>
+            <p>2005 - 2017</p>
+          </div>
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-mortarboard-fill"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">SMAN 7 Surabaya</h3>
+            <p>2018 - 2020</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="pengalaman">
+      <div class="container px-4 py-3" id="featured-3">
+        <h2 class="pb-2 border-bottom">Pengalaman Organisasi</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-people-fill"></i>
+            </div>
+            <h3 class="fs-4 text-body-emphasis">
+              Karang Taruna Kampung Gurami, Perak Barat
+            </h3>
+            <p>2015 - 2017</p>
+          </div>
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-people-fill"></i>
+            </div>
+            <h3 class="fs-4 text-body-emphasis">
+              Paduan Suara SMAN 7 Surabaya
+            </h3>
+            <p>2018 - 2019</p>
+          </div>
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-people-fill"></i>
+            </div>
+            <h3 class="fs-4 text-body-emphasis">
+              Himpunan Mahasiswa Prodi Manajemen Informatika
+            </h3>
+            <p>2021 - 2022</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="prestasi">
+      <div class="container px-4 py-3" id="featured-3">
+        <h2 class="pb-2 border-bottom">Prestasi</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div class="feature col">
+            <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-success bg-gradient fs-2 mb-3 px-2 rounded">
+              <i class="bi bi-stars"></i>
+            </div>
+            <h3 class="fs-2 text-body-emphasis">Non-Akademik</h3>
+            <p>Peserta Paduan Suara HUT RI ke-74, Provinsi Jawa Timur</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="kemampuan">
+      <div class="container px-4 py-3" id="featured-3">
+        <h2 class="pb-2 border-bottom">Kemampuan</h2>
+        <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          <div class="feature col">
+            <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar" style="width: 100%">100%</div>
+            </div>
+            <h3 class="fs-5 text-body-emphasis mt-2">Expert</h3>
+            <p>
+              Mengoperasikan Microsoft Office (Ms.Word, Ms. Excel, Ms. Power
+              Point)
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+
 </html>
